e11df8959b98f239305b6daef26b624f6a02a4f9
skybedy <skybedy@gmail.com>
2022-01-16 11:43:01 +0100

    priprava modelu pro Homepage

---
+++ app/Model/Basic.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Model;
+
+class Basic {
+
+    public $sqlZavody;
+
+    //public function sqlShortcuts($raceYear){
+     //   $this->sqlZavody = 'zavody_'.$raceYear;
+    //}
+
+    protected $x = 1;
+
+    protected function currentYear()
+    {
+        return date("Y");
+    }
+
+    protected function nextYear()
+    {
+        return date("Y") + 1;
+    }
+
+    protected function lastYear()
+    {
+        return date("Y") - 1;
+    }
+
+
+}
+
+
+?>
